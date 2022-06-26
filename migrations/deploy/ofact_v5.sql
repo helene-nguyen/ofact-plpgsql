a48271d5cb710e363633d29a80ba476eb1bc2984
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-06-26 09:13:05 +0200

    [update] ofact v4 tva

--- migrations/deploy/ofact_v5.sql
+++ migrations/deploy/ofact_v5.sql
@@ -2,24 +2,22 @@
 
 BEGIN;
 
-CREATE TYPE
-
 
 --* Use my created view to have expected result
 CREATE VIEW invoice_recap AS
 SELECT 
-invoice_details."Invoice_Ref",
-invoice_details."Date issue",
-invoice_details."Payment date",
-invoice_details."Visitor",
-SUM(invoice_details."Total price per product")::numeric(10,2) AS "Total invoice"
-FROM invoice_details
+I."Invoice_Ref",
+I."Date issue",
+I."Payment date",
+I."Visitor",
+SUM(I."Total price per product")::numeric(10,2) AS "Total invoice"
+FROM invoice_details AS I
 GROUP BY 
-invoice_details."Invoice_Ref",
-invoice_details."Date issue",
-invoice_details."Payment date",
-invoice_details."Visitor"
-ORDER BY invoice_details."Invoice_Ref";
+I."Invoice_Ref",
+I."Date issue",
+I."Payment date",
+I."Visitor"
+ORDER BY I."Invoice_Ref";
 
 -- Search invoice recap :
 --SELECT * FROM invoice_recap;
